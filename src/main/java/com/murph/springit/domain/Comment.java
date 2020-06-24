7fcd37f54c9b99b417fef317022987b32886899a
Hunter Murphy <gritmurphgrit@gmail.com>
2020-06-24 17:59:49 -0400

    Added some comments and listed them on the view

--- src/main/java/com/murph/springit/domain/Comment.java
+++ src/main/java/com/murph/springit/domain/Comment.java
@@ -1,11 +1,18 @@
 package com.murph.springit.domain;
 
+import java.util.Date;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
 
-import lombok.Data;
+import com.murph.springit.service.BeanUtil;
+
+import org.ocpsoft.prettytime.PrettyTime;
+
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.NonNull;
@@ -23,10 +30,20 @@ public class Comment extends Auditable {
 	@Id
 	@GeneratedValue
 	private long id;
+
 	@NonNull
 	private String body;
 
 	@ManyToOne
 	@NonNull
 	private Link link;
+
+	public String getPrettyTime() {
+		PrettyTime pt = BeanUtil.getBean(PrettyTime.class);
+		return pt.format(convertToDateViaInstant(getCreationDate()));
+	}
+	
+	private Date convertToDateViaInstant(LocalDateTime dateToConvert) {
+		return java.util.Date.from(dateToConvert.atZone(ZoneId.systemDefault()).toInstant());
+	}
 }
